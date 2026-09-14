802dd58cc560506971ef254c8f886d9ff474b924
Jebisha <thapajebisha42@gmail.com>
2026-09-14 13:14:38 +0545

    learned: if/else in javascript in peekaboo project

--- peekaboo/script.js
+++ peekaboo/script.js
@@ -1,2 +1,9 @@
 const btn = document.getElementById('btn');
 const para = document.getElementById('paragraph');
+btn.addEventListener('click', ()=>{
+    if (para.style.display ==="none"){
+        para.style.display ="block";
+    }else{
+            para.style.display="none";
+    }
+});
