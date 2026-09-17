810a0ae367cf78fe549ad83212b4a99a3e5bac7c
Jebisha <thapajebisha42@gmail.com>
2026-09-17 18:44:50 +0545

    feat: javascript in largest number program using different functions and arrays

--- Added Number/script.js
+++ Added Number/script.js
@@ -1,1 +1,32 @@
- 
+ const inputValue = document.getElementById("inputValue");
+ const btn = document.getElementById("btn");
+ const outputBox = document.getElementById("output-box");
+ const largestBox = document.getElementById("largest-box");
+ let arr = [];
+ function addedArray(){
+    let input = parseInt(inputValue.value, 10);
+    if(inputValue && !isNaN(input)){
+        arr.push(input);
+    }
+    for (let i=0; i<arr.length; i++){
+        let list = document.createElement("li");
+        list.textContent=arr[i];
+        outputBox.appendChild(list);
+    }
+
+function highestNumber(){
+    let maxNumber = arr[0];
+    for(let i=0; i<arr.length; i++){
+        if (arr[i]>maxNumber){
+            maxNumber=arr[i];
+        }
+
+    }
+    largestBox.textContent=maxNumber;
+
+btn.addEventListener('click',()=>{
+    addedArray();
+
+})
+ }
+}
