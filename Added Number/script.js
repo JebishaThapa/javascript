187d88ff4de5563b16e9f72a4e163431abf4d220
Jebisha <thapajebisha42@gmail.com>
2026-09-17 21:09:29 +0545

    fix: bug fixed in javascript of adding highest number from an array

--- Added Number/script.js
+++ Added Number/script.js
@@ -2,32 +2,45 @@
  const btn = document.getElementById("btn");
  const outputBox = document.getElementById("output-box");
  const largestBox = document.getElementById("largest-box");
- let arr = [];
+ let arr = []; //a new array empty
  function addedArray(){
-    let input = parseInt(inputValue.value, 10);
-    if(isNaN(input)){
-        alert("Please enter a valid number");
-        return;
-        
-    }
-    arr.push(input);
-    inputValue.value="";
+    const input = parseInt(inputValue.value, 10); //made inputvalue in integer kasto? kun wala?
+    if(!isNaN(input)){
+        //if it is not a number error and return back no return? bahira kassari aaux
+        arr.push(input);
+        inputValue.value="";
 
     outputBox.textContent= `Output: [${arr}]`;
+    }else{
+        outputBox.textContent="Enter only Numbers!"
+    }
 
-    highestNumber();
+    
+}
+function clearArray(){
+    const error = inputValue.value;
+    if(error.toLowerCase()==="clear"){
+        outputBox.textContent="Output: cleared";
+        arr=[];
+        inputValue.value="";//aaba feri add garna refresh nai?anii kun hello? how to call 
+        largestBox.textContent="";
+    }
 }
 function highestNumber(){
     let maxNumber = arr[0];
-    for(let i=0; i<arr.length; i++){
+    for(let i=0; i<arr.length; ++i){//how to clear them 
         if (arr[i]>maxNumber){
             maxNumber=arr[i];
         }
 
     }
+    
     largestBox.textContent=`The largest number is: ${maxNumber}`;
 }
+
 btn.addEventListener('click',()=>{
     addedArray();
+    highestNumber();
+    clearArray();
 
 });
