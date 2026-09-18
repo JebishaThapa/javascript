7e4ec8de8c1c499be24f02536f014e25af47ef75
Jebisha <thapajebisha42@gmail.com>
2026-09-18 18:07:52 +0545

    fix: very minor bugs in javascript fixed

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -3,20 +3,23 @@ const listContainer = document.getElementById("list-container");
 function addTask(){
     if(inputBox.value ===""){
         alert("You must write something!");
-    }else{
+    }
+    else{
         let li=document.createElement('li');
         li.innerHTML=inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML="\u00d7";
-        appendChild(span);
-        saveData()
+        li.appendChild(span);
+  
     }
     inputBox.value="";
+    saveData()
+    
 }
 listContainer.addEventListener('click',function (e){
-    if(e.target.tagnName === "LI"){
-        e.target.classList.togglr("checked");
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
         saveData()
 
     }
